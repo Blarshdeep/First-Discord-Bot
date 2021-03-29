0ac46e2dc26e129edcd9254a18926b986764f766
Blarshdeep <blarshgill@gmail.com>
2021-03-29 00:26:20 -0700

    Create main.py
    Bringing bot online

---
+++ main.py
@@ -0,0 +1,11 @@
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = "!")
+
+@client.event
+async def on_ready():
+    print("Bot is online.")
+
+
+client.run(### INSERT TOKEN HERE ###)
