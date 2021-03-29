2e12fadb452d223b86dbf7c5802d7428024c719e
Blarshdeep <blarshgill@gmail.com>
2021-03-29 01:20:43 -0700

    Update main.py
    Added in "!delete", "!kick", "!ban"

--- main.py
+++ main.py
@@ -3,14 +3,31 @@ from discord.ext import commands
 
 client = commands.Bot(command_prefix = "!")
 
+# Bot is ready to go
 @client.event
 async def on_ready():
     print("Bot is online.")
 
+# Checks user's latency
 @client.command()
 async def ping(ctx):
     await ctx.send(f"{round(client.latency * 1000)}ms")
 
+# Deletes messages in channel. Default is the LAST message sent.
+@client.command()
+async def delete(ctx, ammount = 2):
+    await ctx.channel.purge(limit=ammount)
 
+# Kick member
+@client.command()
+async def kick(ctx, member : discord.Member, *, reason=None):
+  await member.kick(reason=reason)
+  await ctx.send(f"{member.mention} has been kicked.")
 
-client.run(### ENTER TOKEN HERE ###)
+# Ban member
+@client.command()
+async def ban(ctx, member : discord.Member, *, reason=None):
+  await member.ban(reason=reason)
+  await ctx.send(f"{member.mention} has been banned.")
+
+client.run(### ENTER BOT TOKEN HERE ###)
